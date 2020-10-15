b4d0a4382ab03e836d5f9f0482d280a597dc32c9
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-10-15 20:47:05 +0300

    Урок 1. Создание фрагмента

---
+++ Lesson_1/src/main/java/com/Lesson_1/MyFragment.java
@@ -0,0 +1,30 @@
+package com.Lesson_1;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_my, container, false);
+
+        Button button = view.findViewById(R.id.button);
+        TextView textView = view.findViewById(R.id.tv);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("" + Math.random());
+            }
+        });
+
+        return view;
+    }
+}
